188eb719b76db63d3db33ac0b3df8aecce17cc44
nabilbachroin <abiel769@gmail.com>
2023-11-11 15:54:00 +0800

    first commit

---
+++ ordersystem_django/ordersystem_django/asgi.py
@@ -0,0 +1,20 @@
+"""
+ASGI config for ordersystem_django project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ordersystem_django.settings')
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  # (Tambahkan rute WebSocket di sini jika ada)
+})
